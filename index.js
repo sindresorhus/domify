44b94967a1a5329ffe2025d8efa4a3d00e08d7ee
TJ Holowaychuk <tj@vision-media.ca>
2012-08-12 16:53:50 -0700

    add component.json

--- index.js
+++ index.js
@@ -1,2 +1,69 @@
 
-module.exports = require('./lib/domify');
+/**
+ * Wrap map from jquery.
+ */
+
+var map = {
+  option: [1, '<select multiple="multiple">', '</select>'],
+  optgroup: [1, '<select multiple="multiple">', '</select>'],
+  legend: [1, '<fieldset>', '</fieldset>'],
+  thead: [1, '<table>', '</table>'],
+  tbody: [1, '<table>', '</table>'],
+  tfoot: [1, '<table>', '</table>'],
+  colgroup: [1, '<table>', '</table>'],
+  caption: [1, '<table>', '</table>'],
+  tr: [2, '<table><tbody>', '</tbody></table>'],
+  td: [3, '<table><tbody><tr>', '</tr></tbody></table>'],
+  th: [3, '<table><tbody><tr>', '</tr></tbody></table>'],
+  col: [2, '<table><tbody></tbody><colgroup>', '</colgroup></table>'],
+  _default: [0, '', '']
+};
+
+/**
+ * Convert the given `html` into DOM elements.
+ *
+ * @api public
+ */
+
+module.exports = function(html){
+  if ('string' != typeof html) throw new TypeError('String expected');
+
+  // tag name
+  var m = /<([\w:]+)/.exec(html);
+  if (!m) throw new Error('No elements were generated.');
+  var tag = m[1];
+
+  // body support
+  if (tag == 'body') {
+    var el = document.createElement('body');
+    el.innerHTML = stripBody(html);
+    return el;
+  }
+
+  // wrap map
+  var wrap = map[tag] || map._default;
+  var depth = wrap[0];
+  var prefix = wrap[1];
+  var suffix = wrap[2];
+  var el = document.createElement('div');
+  el.innerHTML = prefix + html + suffix;
+  while (depth--) el = el.lastChild;
+
+  if (el.lastChild.nextElementSibling || el.lastChild.previousElementSibling) {
+    throw new Error('More than one element was generated.');
+  }
+
+  return el.lastChild;
+};
+
+/**
+ * Strip body tags from `html`.
+ *
+ * @param {String} html
+ * @return {String}
+ * @api private
+ */
+
+function stripBody(html) {
+  return html.replace(/^\s*<body[^>]*>/, '').replace(/<\/body>\s*$/, '');
+}
