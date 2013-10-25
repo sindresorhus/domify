3c9869450b254297e621a8241b910b7a98d796f9
Tim <secoif@gmail.com>
2013-10-25 10:52:26 +0800

    Do not rely on live .children NodeList.
    Makes domify work with polymer ShadowDOM polyfill.

--- index.js
+++ index.js
@@ -59,14 +59,13 @@ function parse(html) {
   el.innerHTML = prefix + html + suffix;
   while (depth--) el = el.lastChild;
 
-  var els = el.children;
-  if (1 == els.length) {
-    return el.removeChild(els[0]);
+  if (1 == el.children.length) {
+    return el.removeChild(el.children[0]);
   }
 
   var fragment = document.createDocumentFragment();
-  while (els.length) {
-    fragment.appendChild(el.removeChild(els[0]));
+  while (el.children.length) {
+    fragment.appendChild(el.removeChild(el.children[0]));
   }
 
   return fragment;
